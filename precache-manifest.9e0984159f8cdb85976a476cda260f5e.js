b2f5590a08b016a4c5f55864ca648f63bbc3b512
Oleksandr Radchenko <radchenko.oskh@gmail.com>
2020-07-28 13:16:02 +0300

    init web

---
+++ precache-manifest.9e0984159f8cdb85976a476cda260f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5329ce7c69132af679658739c0d303d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a902c946f45daec2eca",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da65c3a014ce95bacd55",
+    "url": "/static/js/2.15b423a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15b423a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a902c946f45daec2eca",
+    "url": "/static/js/main.5cf618f7.chunk.js"
+  },
+  {
+    "revision": "68272b1c9212ff8b5b64",
+    "url": "/static/js/runtime-main.53ee41a6.js"
+  }
+]);
